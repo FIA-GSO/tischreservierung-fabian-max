0703188f4720cdf3d3a4038e1d187b16802a78f9
Fabian Kohlhaas <fabian@kohlhaas.email>
2023-11-28 14:29:50 +0100

    Add first api tests

--- api/test_api.py
+++ api/test_api.py
@@ -12,4 +12,20 @@ def test_tischreservierung(app):
     route = "/"
     response = app.get(route)
     assert response.status_code == 200
-    assert b"<h1>Tischreservierung</h1>" in response.data   
+    assert b"<h1>Tischreservierung</h1>" in response.data
+
+# def test_reservations__all(app):
+#     route = "/api/v1/reservations" 
+#     expected =b'{"pin":1331,"reservierungsnummer":1,"storniert":"False","tischnummer":1,"zeitpunkt":"2022-02-02 17:30:00"}'
+
+#     response = app.get(route)
+#     assert response.status_code == 200
+#     assert expected in response.data
+
+def test_reservations__all(app):
+    route = "/api/v1/reservations" 
+    expected =b'pin":'
+
+    response = app.get(route)
+    assert response.status_code == 200
+    assert expected in response.data
